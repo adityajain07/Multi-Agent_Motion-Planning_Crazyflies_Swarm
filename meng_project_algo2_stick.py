c054ebd8b3359a3b6b1105bcb8d4ca32e685588e
Aditya Jain <aditya14129@iiitd.ac.in>
2023-04-04 22:06:01 -0400

    Experiments on April 3, 2023

--- meng_project_algo2_stick.py
+++ meng_project_algo2_stick.py
@@ -3,21 +3,30 @@ from __future__ import print_function
 
 from pycrazyswarm import *
 import sys
+import numpy as np
 import rospy
 from geometry_msgs.msg import TransformStamped
+import time
 
 class CrazyflieObstacleAvoidance(object):
     """ROS interface for avoiding obstacles in real time for crazyflie swarm"""
 
     def __init__(self):
         # subscriber to obstacle's vicon data
-        sub_vicon_data = rospy.Subscriber('/vicon/danda/danda',
+        sub_vicon_data = rospy.Subscriber('/vicon/glove_right/glove_right',
                                             TransformStamped,
                                             self.update_obstacle_position)
-        # obstacle position
-        self.obs_x = 0.0
-        self.obs_y = 0.0
-        self.obs_z = 0.0
+        # object position position
+        self.cur_object_x = 0.0
+        self.cur_object_y = 0.0
+        self.cur_object_z = 0.0
+        
+        # self.last_object_x = 0.0
+        # self.last_object_y = 0.0
+        # self.last_object_z = 0.0
+
+        # TO ADD COMMENT
+        self.threshold = 0.01
 
         # variables for crazyflies
         self.swarm = Crazyswarm()
@@ -39,9 +48,9 @@ class CrazyflieObstacleAvoidance(object):
 
     def update_obstacle_position(self, vicon_data):
         """updates the obstacle's position globally in the function"""
-        self.obs_x = vicon_data.transform.translation.x
-        self.obs_y = vicon_data.transform.translation.y
-        self.obs_z = vicon_data.transform.translation.z
+        self.cur_object_x = vicon_data.transform.translation.x
+        self.cur_object_y = vicon_data.transform.translation.y
+        self.cur_object_z = vicon_data.transform.translation.z
 
     def takeoff_crazyflies(self):
         """taking off crazyflies"""
@@ -57,14 +66,37 @@ class CrazyflieObstacleAvoidance(object):
 
     def command_crazyflies(self, event=None):
         """main function for controlling the crazyflies"""
-
-        while (self.timePassed<15):
+        last_object_position = np.array([self.cur_object_x , self.cur_object_y , self.cur_object_z])
+        start_time = time.time()
+        
+        while ((time.time()-start_time)<20):
+            current_object_position  = np.array([self.cur_object_x , self.cur_object_y , self.cur_object_z])
+            diff = current_object_position - last_object_position
+            diff_x = 0.0
+            diff_y = 0.0
+            diff_z = 0.0
+            position_changed = False
             for cf in self.allcfs.crazyflies:
-                print(f'Drone {cf.id}s X position is: {cf.position()[0]}, Y position is: {cf.position()[1]}, Z position is: {cf.position()[2]}')
-                print(f'Objects X position is: {self.obs_x}, Y position is: {self.obs_y}, Z position is: {self.obs_z}')
-                cf.cmdPosition([cf.position()[0], cf.position()[1], self.obs_z])
-                self.timeHelper.sleep(0.1)
-            self.timePassed += 0.1
+                cf_x = cf.position()[0]; cf_y = cf.position()[1]; cf_z = cf.position()[2]; 
+                if (abs(diff[0])>self.threshold):
+                    diff_x = diff[0]
+                    last_object_position[0] = last_object_position[0] + diff[0]
+                    position_changed = True
+                if (abs(diff[1])>self.threshold):
+                    diff_y = diff[1]
+                    last_object_position[1] = last_object_position[1] + diff[1]
+                    position_changed = True
+                if (abs(diff[2])>self.threshold):
+                    diff_z = diff[2]
+                    last_object_position[2] = last_object_position[2] + diff[2]
+                    position_changed = True
+
+                if position_changed:
+                    cf.goTo([cf_x+diff_x, cf_y+diff_y , cf_z+diff_z],0, duration = 1.0)
+                
+                print('Last Object Position:', last_object_position)
+            self.timeHelper.sleep(0.5)
+
         self.land_crazyflies()
         sys.exit()
 
@@ -72,3 +104,5 @@ if __name__ == "__main__":
     # rospy.init_node('command_crazyflies')
     CrazyflieObstacleAvoidance()
     rospy.spin()
+
+
